1fa73dba96ba7d9f1779d59a5e36472ccc735b17
Valerie Yeo <valerie@Valeries-MacBook-Pro.local>
2025-07-24 23:08:52 +0800

    readding more files

---
+++ PingMe/PingMeTests/PingMeTests.swift
@@ -0,0 +1,17 @@
+//
+//  PingMeTests.swift
+//  PingMeTests
+//
+//  Created by Valerie Yeo on 31/5/25.
+//
+
+import Testing
+@testable import PingMe
+
+struct PingMeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
